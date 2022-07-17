889be558367c2386db56b926de7926c9d92abc04
ext_dlisiecki <dennis.lisiecki@uol.de>
2022-07-18 00:53:02 +0200

    Add copyright

--- tms.py
+++ tms.py
@@ -1,3 +1,20 @@
+# TMS - Testing Finite-Memory Strategies for Petri Games
+# Copyright (C) 2022 Dennis Lisiecki
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# For questions regarding how to use the program, refer to the manual in the github repository Lisiecki/tms
 import sys, getopt
 import copy
 import config
